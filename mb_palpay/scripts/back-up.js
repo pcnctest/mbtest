c4f66c82329cdcb97ba52bcc5ddd6b88e29e4517
PCNC Solutions <pcncdeveloper@gmail.com>
2013-12-23 02:33:54 -0600

    Changes made by PCNC Solutions (pcncdeveloper@gmail.com).

--- mb_palpay/scripts/back-up.js
+++ mb_palpay/scripts/back-up.js
@@ -1,1 +1,18 @@
-﻿
+ $.getJSON("data/newCustomerData.json", function(data) {
+            var moreAccountInfo = [];
+                 
+            $.each(data.accounts, function(key, val) {
+                var $accountInfo = $(this).get(0);     
+                if ($accountInfo.accountNumber===curAccount) {
+                    //add content here 
+                    moreAccountInfo.push("<li><strong>Account Number: </strong><br>" + $accountInfo.accountNumber + "</li>" +
+                                         "<li><strong>IBAN_BBAN :</strong>" + $accountInfo.IBAN_BBAN + "</li>" +
+                                         "<li><strong>Type: </strong>" + $accountInfo.type + "</li>" +
+                                         "<li><strong>Available Balance :</strong>" + $accountInfo.availableBalance + "</li>" +
+                                         "<li><strong>Current Balance : </strong>" + $accountInfo.currentBalance + "</li>" +
+                                         "<li><strong>Currancy :</strong>" + $accountInfo.currency + "</li>");
+                }
+                //$("#moreInfoDiv").html(moreAccountInfo);  
+            });
+            //$('#balanceInquiryTable').html(bankAccounts);
+        });
